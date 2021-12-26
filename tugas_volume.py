09bcee845ea236a0036750fb0ba7e4f463ea4cb2
Hapidr <67416357+Hapidr@users.noreply.github.com>
2021-12-26 08:56:19 +0700

    Update tugas_volume.py
    add a default arguments

--- tugas_volume.py
+++ tugas_volume.py
@@ -1,21 +1,21 @@
-def volume_tabung(jari_jari, tinggi):
+def volume_tabung(jari_jari = 8, tinggi = 10):
     print(f"Jari-jari tabung {jari_jari}, tinggi {tinggi}".title())
     if jari_jari % 7 == 0:
         return (22/7) * (jari_jari**2) * tinggi
     else:
         return 3.14 * (jari_jari**2) * tinggi
 
-def volume_kerucut(jari_jari, tinggi):
+def volume_kerucut(jari_jari = 7, tinggi = 10):
     print(f"Jari-jari kerucut {jari_jari}, tinggi {tinggi}".title())
     if jari_jari % 7 == 0:
         return (1/3) * ((22/7) * (jari_jari**2) * tinggi)
     else:
         return (1/3) * ((22/7) * (jari_jari**2) * tinggi)
 
-def volume_kubus(rusuk):
+def volume_kubus(rusuk = 7):
     print(f"Rusuk kerucut {rusuk}".title())
     return rusuk**3
 
-print(volume_tabung(8, 10))
-print(volume_kerucut(7, 10))
-print(volume_kubus(7))
+print(volume_tabung())
+print(volume_kerucut())
+print(volume_kubus())
