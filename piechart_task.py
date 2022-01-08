8dd065a0ebac33962bf4516518420d5c9699f540
Hapidr <67416357+Hapidr@users.noreply.github.com>
2022-01-08 12:53:07 +0700

    Add files via upload

---
+++ piechart_task.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('D:\Downloads\Data.csv')
+new_df = df.groupby('Country').sum()
+# print(df)
+print(new_df)
+
+
+death = [new_df['Death'][0], new_df['Death'][1], new_df['Death'][2]]
+labels = ['Bangladesh', 'India', 'Pakistan']
+plt.pie(death, labels=labels)
+plt.show()
