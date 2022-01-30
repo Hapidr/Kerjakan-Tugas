3b58896b004ce05d87a275c47a3e34e84b216b20
Hapidr <67416357+Hapidr@users.noreply.github.com>
2022-01-30 09:49:43 +0700

    Update 50_hari.py
    - Changed the day variable from list type to dictionary type
    - Updating print on line 3
    - Added conditional to check Day input value

--- 50_hari.py
+++ 50_hari.py
@@ -1,9 +1,17 @@
-print("PROGRAM MENGHITUNG 50 HARI KEMUDIAN\n ")
-hari = ["Senin", "Selasa", "Rabu", "Kamis", "Jum'at" , "Sabtu", "Minggu"]
+print("PROGRAM MENGHITUNG 50 HARI KEMUDIAN".center(50))
+hari = {1: "Senin", 2: "Selasa", 3: "Rabu", 4: "Kamis", 5: "Jumat", 6: "Sabtu", 7: "Minggu"}
+print("1. Senin\n2. Selasa\n3. Rabu\n4. Kamis\n5. Jum'at\n6. Sabtu\n7. Minggu")
 
-print("0. Senin\n1. Selasa\n2. Rabu\n3. Kamis\n4. Jum'at\n5. Sabtu\n6. Minggu")
+day = int(input("Masukkan Angka 1-7: "))
+if day > 6 and day <= 7:
+    hi = day * 0
+    yaum = hi + (50 % 7)
+    print(f"Hari ke-{day} adalah {hari[day]}")
+    print(f"50 Hari setelah hari {hari[day]} adalah hari {hari[yaum]}")
 
-day = int(input("Masukkan hari ini: "))
-
-yaum = day + (50 % 7)
-print("50 Hari setelah hari ", hari[day], " adalah hari", hari[yaum])
+elif day > 0 and day <= 6:
+    yaum = day + (50 % 7)
+    print(f"Hari ke-{day} adalah {hari[day]}")
+    print(f"50 Hari setelah hari {hari[day]} adalah hari {hari[yaum]}")
+else:
+    print("Angka yang anda masukkan tidak sesuai")
